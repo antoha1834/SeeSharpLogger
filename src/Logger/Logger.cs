80aaf70f3b46b9d88879b1b3d9fbfa8d1788534c
antoha1834 <154402909+antoha1834@users.noreply.github.com>
2025-06-13 21:34:25 +0800

    now i sure that i added namespace

--- src/Logger/Logger.cs
+++ src/Logger/Logger.cs
@@ -1,22 +1,27 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Text;
 
-/// <summary>
-/// LogState specifies a pair for prefix and color
-/// </summary>
-public enum LogState
+namespace SeeSharpLogger
 {
-    Log,
-    Unimportant,
-    Error,
-    Warning,
-    Success,
-    Info
-}
+    /// <summary>
+    /// LogState specifies a pair for prefix and color
+    /// </summary>
+    public enum LogState
+    {
+        Log,
+        Unimportant,
+        Error,
+        Warning,
+        Success,
+        Info
+    }
 
-internal static class Maps
-{
-    public static readonly Dictionary<LogState, string> prefixMap = new()
+    internal static class Maps
+    {
+        public static readonly Dictionary<LogState, string> prefixMap = new()
     {
         {LogState.Log, "[*] "},
         {LogState.Error, "[X] "},
@@ -26,7 +31,7 @@ internal static class Maps
         {LogState.Unimportant, "[-] "}
     };
 
-    public static readonly Dictionary<LogState, ConsoleColor> colorMap = new()
+        public static readonly Dictionary<LogState, ConsoleColor> colorMap = new()
     {
         {LogState.Log, ConsoleColor.White},
         {LogState.Error, ConsoleColor.Red},
@@ -35,200 +40,203 @@ internal static class Maps
         {LogState.Info, ConsoleColor.Cyan},
         {LogState.Unimportant, ConsoleColor.DarkGray}
     };
-}
-
-public class Log
-{
-    public string Name { get; set; }
-
-    /// <summary>
-    /// Default source for static calls of Write() / WriteLine()
-    /// </summary>
-    public static string DefaultSource { get; set; } = "Anonymous";
-
-    /// <summary>
-    /// Prefix for unknown LogState (you will never see it... at least i hope)
-    /// </summary>
-    public static string DefaultPrefix { get; set; } = "[?] ";
-
-    /// <summary>
-    /// Should the log be written to a file?
-    /// </summary>
-    public static bool LogToFile { get => _logToFile; }
-    private static bool _logToFile = false;
-
-    public static bool AddTimeStamp { get; set; } = true;
-
-    private static FileStream? _logFile;
-
-    /// <summary>
-    /// Create new Log instance with only name specified (without logging to the file)
-    /// </summary>
-    /// <param name="name">source</param>
-    public Log(string name) => Name = name;
-
-    /// <summary>
-    /// Create new Log instance with only name specified (with logging to the file)
-    /// </summary>
-    /// <param name="name">source that would be added to formatted log</param>
-    /// <param name="path">directory for the .log files</param>
-    /// <param name="format">format for the .log file name</param>
-    /// <param name="timeSplitter">time splitter (replace of ":")</param>
-    /// <param name="addTimeStamp">should a timestamp be added to each message</param>
-    public static void EnableLogToFile(string path, string format, bool addTimeStamp = true, string timeSplitter = "-")
-    {
-        _logToFile = true;
-        AddTimeStamp = addTimeStamp;
-
-        SetPath(path, format, timeSplitter);
     }
 
-    public static void DisableLogToFile()
+#nullable enable
+    public class Log
     {
-        _logToFile = false;
-        AddTimeStamp = false;
+        public string Name { get; set; }
 
-        _logFile?.Flush();
-        _logFile?.Dispose();
-    }
+        /// <summary>
+        /// Default source for static calls of Write() / WriteLine()
+        /// </summary>
+        public static string DefaultSource { get; set; } = "Anonymous";
 
-    /// <summary>
-    /// Sets the .log file path
-    /// </summary>
-    /// <param name="path">directory for the .log files</param>
-    /// <param name="format">format for the .log file name</param>
-    /// <param name="timeSplitter">time splitter (replace of ":")</param>
-    private static void SetPath(string path, string format, string timeSplitter = "-")
-    {
-        Directory.CreateDirectory(path);
+        /// <summary>
+        /// Prefix for unknown LogState (you will never see it... at least i hope)
+        /// </summary>
+        public static string DefaultPrefix { get; set; } = "[?] ";
 
-        if (!path.EndsWith('\\') && !path.EndsWith('/'))
+        /// <summary>
+        /// Should the log be written to a file?
+        /// </summary>
+        public static bool LogToFile { get => _logToFile; }
+        private static bool _logToFile = false;
+
+        public static bool AddTimeStamp { get; set; } = true;
+
+        private static FileStream? _logFile;
+
+        /// <summary>
+        /// Create new Log instance with only name specified (without logging to the file)
+        /// </summary>
+        /// <param name="name">source</param>
+        public Log(string name) => Name = name;
+
+        /// <summary>
+        /// Create new Log instance with only name specified (with logging to the file)
+        /// </summary>
+        /// <param name="name">source that would be added to formatted log</param>
+        /// <param name="path">directory for the .log files</param>
+        /// <param name="format">format for the .log file name</param>
+        /// <param name="timeSplitter">time splitter (replace of ":")</param>
+        /// <param name="addTimeStamp">should a timestamp be added to each message</param>
+        public static void EnableLogToFile(string path, string format, bool addTimeStamp = true, string timeSplitter = "-")
         {
-            path += "\\";
+            _logToFile = true;
+            AddTimeStamp = addTimeStamp;
+
+            SetPath(path, format, timeSplitter);
         }
 
-        string fileName = ParseFormat(format, timeSplitter);
-        string fullPath = Path.Combine(path, fileName);
-
-        _logFile = new FileStream(fullPath, FileMode.Create, FileAccess.Write, FileShare.Read);
-        AppDomain.CurrentDomain.ProcessExit += EndLoggerWork;
-    }
-
-    private static void EndLoggerWork(object? sender, EventArgs e)
-    {
-        if (_logToFile)
-        DisableLogToFile();
-    }
-
-    private static string ParseFormat(string input, string timeSplitter)
-    {
-        return input.Replace("<Date>", DateTime.Now.ToString(CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern))
-                    .Replace("<Time>", DateTime.Now.ToString(CultureInfo.CurrentCulture.DateTimeFormat.ShortTimePattern).Replace(":", timeSplitter));
-    }
-
-    private static void AddText(FileStream? fs, string value)
-    {
-        byte[] info = new UTF8Encoding(true).GetBytes(value);
-        fs?.Write(info, 0, info.Length);
-    }
-
-    private static readonly object _rawLock = new();
-
-    // base for colored + prefixed writing
-    private static void RawWrite(string prefix = "", string from = "", string? message = null, ConsoleColor color = ConsoleColor.White)
-    {
-        lock (_rawLock)
+        public static void DisableLogToFile()
         {
-            Console.ForegroundColor = color;
-            Console.Write($"{prefix}{from}{message}");
+            _logToFile = false;
+            AddTimeStamp = false;
 
-            if (LogToFile)
-                AddText(_logFile, $"{prefix}{from}{message}");
-
-            Console.ResetColor();
+            _logFile?.Flush();
+            _logFile?.Dispose();
         }
-    }
 
-    // base for correct writing
-    private static void CoreWrite(object? message, string from = "Anonymous", LogState state = LogState.Log)
-    {
-        if (Maps.prefixMap.TryGetValue(state, out var prefix) && Maps.colorMap.TryGetValue(state, out var color))
+        /// <summary>
+        /// Sets the .log file path
+        /// </summary>
+        /// <param name="path">directory for the .log files</param>
+        /// <param name="format">format for the .log file name</param>
+        /// <param name="timeSplitter">time splitter (replace of ":")</param>
+        private static void SetPath(string path, string format, string timeSplitter = "-")
         {
-            RawWrite(AddTimeStamp ? $"[{GetTimeStamp()}] {prefix}" : prefix, $"[{from}] ", message?.ToString(), color);
+            Directory.CreateDirectory(path);
+
+            if (!path.EndsWith('\\') && !path.EndsWith('/'))
+            {
+                path += "\\";
+            }
+
+            string fileName = ParseFormat(format, timeSplitter);
+            string fullPath = Path.Combine(path, fileName);
+
+            _logFile = new FileStream(fullPath, FileMode.Create, FileAccess.Write, FileShare.Read);
+            AppDomain.CurrentDomain.ProcessExit += EndLoggerWork;
         }
-        else
+
+        private static void EndLoggerWork(object? sender, EventArgs e)
         {
-            RawWrite(DefaultPrefix, DefaultSource, message?.ToString(), ConsoleColor.DarkGray);
+            if (_logToFile)
+                DisableLogToFile();
         }
-    }
 
-    public static string GetTimeStamp()
-    {
-        return DateTime.Now.ToString(CultureInfo.CurrentCulture.DateTimeFormat.FullDateTimePattern);
-    }
+        private static string ParseFormat(string input, string timeSplitter)
+        {
+            return input.Replace("<Date>", DateTime.Now.ToString(CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern))
+                        .Replace("<Time>", DateTime.Now.ToString(CultureInfo.CurrentCulture.DateTimeFormat.ShortTimePattern).Replace(":", timeSplitter));
+        }
 
-    /// <summary>
-    /// Writes a new line with the specified LogState
-    /// </summary>
-    public void WriteLine(object? message = null, LogState state = LogState.Log)
-    {
-        CoreWrite(message + Environment.NewLine, Name, state);
-    }
+        private static void AddText(FileStream? fs, string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fs?.Write(info, 0, info.Length);
+        }
 
-    /// <summary>
-    /// Writes a new line with the specified LogState and source
-    /// </summary>
-    public static void WriteLine(object? message = null, string? from = null, LogState state = LogState.Log)
-    {
-        from ??= DefaultSource;
-        CoreWrite(message + Environment.NewLine, from, state);
-    }
+        private static readonly object _rawLock = new();
 
-    /// <summary>
-    /// Writes a string without prefixes and line break
-    /// </summary>
-    /// <param name="message">string to write</param>
-    /// <param name="color">color of stringto write</param>
-    public static void Raw(object? message = null, ConsoleColor color = ConsoleColor.White)
-    {
-        RawWrite("", "", message?.ToString(), color);
-    }
+        // base for colored + prefixed writing
+        private static void RawWrite(string prefix = "", string from = "", string? message = null, ConsoleColor color = ConsoleColor.White)
+        {
+            lock (_rawLock)
+            {
+                Console.ForegroundColor = color;
+                Console.Write($"{prefix}{from}{message}");
 
-    /// <summary>
-    /// Writes new line with specified color and without prefixes
-    /// </summary>
-    /// <param name="message"></param>
-    /// <param name="color"></param>
-    public static void RawLine(object? message, ConsoleColor color = ConsoleColor.White)
-    {
-        RawWrite("", "", message?.ToString() + Environment.NewLine, color);
-    }
+                if (LogToFile)
+                    AddText(_logFile, $"{prefix}{from}{message}");
 
-    /// <summary>
-    /// Go to next line without message
-    /// </summary>
-    public static void SkipLine()
-    {
-        RawWrite("", "", Environment.NewLine);
-    }
+                Console.ResetColor();
+            }
+        }
 
-    /// <summary>
-    /// Globally changes the prefix for one LogState
-    /// </summary>
-    /// <param name="state">LogState to change prefix</param>
-    /// <param name="newPrefix">new prefix</param>
-    public static void SetStatePrefix(LogState state, string newPrefix)
-    {
-        Maps.prefixMap[state] = newPrefix;
-    }
+        // base for correct writing
+        private static void CoreWrite(object? message, string from = "Anonymous", LogState state = LogState.Log)
+        {
+            if (Maps.prefixMap.TryGetValue(state, out var prefix) && Maps.colorMap.TryGetValue(state, out var color))
+            {
+                RawWrite(AddTimeStamp ? $"[{GetTimeStamp()}] {prefix}" : prefix, $"[{from}] ", message?.ToString(), color);
+            }
+            else
+            {
+                RawWrite(DefaultPrefix, DefaultSource, message?.ToString(), ConsoleColor.DarkGray);
+            }
+        }
 
-    /// <summary>
-    /// Globally changes the color for one LogState
-    /// </summary>
-    /// <param name="state">LogState to change color</param>
-    /// <param name="newColor">new color</param>
-    public static void SetStateColor(LogState state, ConsoleColor newColor)
-    {
-        Maps.colorMap[state] = newColor;
+        public static string GetTimeStamp()
+        {
+            return DateTime.Now.ToString(CultureInfo.CurrentCulture.DateTimeFormat.FullDateTimePattern);
+        }
+
+        /// <summary>
+        /// Writes a new line with the specified LogState
+        /// </summary>
+        public void WriteLine(object? message = null, LogState state = LogState.Log)
+        {
+            CoreWrite(message + Environment.NewLine, Name, state);
+        }
+
+        /// <summary>
+        /// Writes a new line with the specified LogState and source
+        /// </summary>
+
+        public static void WriteLine(object? message = null, string? from = null, LogState state = LogState.Log)
+        {
+            from ??= DefaultSource;
+            CoreWrite(message + Environment.NewLine, from, state);
+        }
+
+        /// <summary>
+        /// Writes a string without prefixes and line break
+        /// </summary>
+        /// <param name="message">string to write</param>
+        /// <param name="color">color of stringto write</param>
+        public static void Raw(object? message = null, ConsoleColor color = ConsoleColor.White)
+        {
+            RawWrite("", "", message?.ToString(), color);
+        }
+
+        /// <summary>
+        /// Writes new line with specified color and without prefixes
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="color"></param>
+        public static void RawLine(object? message, ConsoleColor color = ConsoleColor.White)
+        {
+            RawWrite("", "", message?.ToString() + Environment.NewLine, color);
+        }
+
+        /// <summary>
+        /// Go to next line without message
+        /// </summary>
+        public static void SkipLine()
+        {
+            RawWrite("", "", Environment.NewLine);
+        }
+
+        /// <summary>
+        /// Globally changes the prefix for one LogState
+        /// </summary>
+        /// <param name="state">LogState to change prefix</param>
+        /// <param name="newPrefix">new prefix</param>
+        public static void SetStatePrefix(LogState state, string newPrefix)
+        {
+            Maps.prefixMap[state] = newPrefix;
+        }
+
+        /// <summary>
+        /// Globally changes the color for one LogState
+        /// </summary>
+        /// <param name="state">LogState to change color</param>
+        /// <param name="newColor">new color</param>
+        public static void SetStateColor(LogState state, ConsoleColor newColor)
+        {
+            Maps.colorMap[state] = newColor;
+        }
     }
 }
